84ffdd8b072cf34165ae7e6941aedf8ed1b7c761
JoaoVitor2547 <joaovitorf1611@gmail.com>
2025-10-20 14:48:10 -0300

    Create class Discipline

---
+++ Discipline.java
@@ -0,0 +1,22 @@
+public class Discipline{
+    private String codigo;
+    private String nome;
+
+    public Discipline(String codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    @Override
+    public String toString() {
+        return codigo + " - " + nome;
+    }
+}
