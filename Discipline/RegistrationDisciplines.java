a7ddfcd030dd10d0be661e800f2cad0566eb535a
João Vitor Fernandes de Sousa <joaovitorf1611@gmail.com>
2025-10-21 10:17:23 -0300

    Update RegistrationDisciplines.java

--- Discipline/RegistrationDisciplines.java
+++ Discipline/RegistrationDisciplines.java
@@ -1,5 +1,23 @@
 package Discipline;
 
-public class RegistrationDisciplines {
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+public class RegistrationDisciplines {
+    
+    private Set<Discipline> Discipline = new LinkedHashSet<>();
+    private Set<Discipline> duplicateDiscipline = new LinkedHashSet<>(); 
+
+    public Set<Discipline> getDuplicateDiscipline() {
+        return duplicateDiscipline;
+    }
+
+    public boolean adicionarDiscipline (Discipline d) { 
+    
+        if (!Discipline.add(d)) {
+            duplicateDiscipline.add(d);
+            return false;
+        }
+        return true;
+    }
 }
