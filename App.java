ec2d1b9dfdb48b79d633bb02c8ebf7607cd047d9
Luiz Matheus <luizmatheus9570@gmail.com>
2025-10-21 00:18:04 -0300

    Criação main e lista de estudantes

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+  public static void main (String[] args) {
+    StudentList students = new StudentList();
+    Student student = new Student(1, "Luiz Matheus Soares");
+    Student student1 = new Student(2, "José Torres Neto");
+    Student student2 = new Student(3, "José Roberto");
+    Student student3 = new Student(4, "Luiz Henrique");
+    students.addStudent(student);
+    students.addStudent(student1);
+    students.addStudent(student2);
+    students.addStudent(student3);
+    System.out.println(students.sortStudentsByName());
+  }
+}
